1c19175000046fbe79d0d6f51ce3c6358c7527ae
xinge0721 <2319830499@qq.com>
2025-10-05 11:35:19 +0800

    初始化ROS

---
+++ Software/Hive_control_ROS/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/xinge/chenxu/DroneSwarm/Software/Hive_control_ROS/src
+# Build directory: /home/xinge/chenxu/DroneSwarm/Software/Hive_control_ROS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
