fc42fbbd6c720203f542430139e174206f80a836
辛格 <2319830499@qq.com>
2025-10-07 12:04:51 +0800

    初始化LED

---
+++ Software/UAV_ESP/Hardware/bluetooth/bluetooth.h
@@ -0,0 +1,13 @@
+#ifndef BLUETOOTH_H
+#define BLUETOOTH_H
+
+#include "bluetooth.h"
+
+static const char *TAG = "Bluetooth";
+
+Bluetooth::Bluetooth()
+{
+    ESP_LOGI(TAG, "蓝牙对象创建完成");
+}
+
+#endif
